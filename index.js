71ac0d4764568605ea5bf87e00d2764197c5ff86
Rafli Ilham Aditia <ilhamrafli732@gmail.com>
2025-01-17 23:45:53 +0700

    change code index.js root

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import router from './routes/index.js';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+const PORT = process.env.PORT || 8080;
+
 dotenv.config();
 const app = express()
 
@@ -20,4 +22,9 @@ app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(router)
-app.listen(5000, ()=> console.log('Server running at port 5000'))
+app.get('/', (req, res) => {
+    res.send('Hello from Cloudrun!');
+});
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`)
+  })
